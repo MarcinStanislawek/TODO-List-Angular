9d98a119204c78e2c8888aa89857d5bcb0345cee
Marcin Stanisławek <mstanislawek@gmail.com>
2020-01-17 20:18:01 +0100

    Added remove tasks and mark as done functiolities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,24 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   newTask: string;
-  tasksList: Array<string> = []
+  tasksList: Array<string> = [];
+  doneTasksList: Array<string> = [];
 
   add() {
     this.tasksList.push(this.newTask);
     this.newTask = '';
     console.log(this.tasksList);
+
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+    console.log(this.tasksList);
+  }
+
+  done(task: string) {
+    this.doneTasksList.push(task);
+    this.remove(task);
 
   }
 }
