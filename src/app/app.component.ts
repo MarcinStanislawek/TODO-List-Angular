4935e9192b773d58c3480191d016b8c20dc98435
Marcin Stanisławek <mstanislawek@gmail.com>
2020-01-17 20:07:14 +0100

    Added html and implemented functionality that let add tasks and show them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TODO-List-Angular';
+
+  newTask: string;
+  tasksList: Array<string> = []
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+
+  }
 }
